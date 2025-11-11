82e6595ae069ea16854707ebe4b5c60b3572fc43
Facundo Soria <facundo.cba2@gmail.com>
2025-11-10 23:49:41 -0300

    Niggers

--- Pharm-api/.dockers/sqlserver/configure-db.sh
+++ Pharm-api/.dockers/sqlserver/configure-db.sh
@@ -37,6 +37,8 @@ echo "Ejecutando scripts de inicialización..."
 # Crear procedimientos almacenados
 /opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d PharmDB -i /opt/mssql-scripts/04-procedures.sql -C
 
+/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d PharmDB -i /opt/mssql-scripts/05-triggers.sql -C
+
 echo "Configuración completada. SQL Server listo para recibir conexiones."
 
 # Mantener el contenedor corriendo
